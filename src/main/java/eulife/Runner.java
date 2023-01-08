e29b005ec4a89bfe7d859d024471d142da1b1f9b
Andrii Liashenko <a.liashenko@e-u.edu.ua>
2023-01-08 11:11:33 +0200

    initial

---
+++ src/main/java/eulife/Runner.java
@@ -0,0 +1,33 @@
+package eulife;
+
+import eulife.dao.implementation.UserDAO;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@SpringBootApplication
+public class Runner {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(Runner.class, args);
+        UserDAO userDAO = context.getBean(UserDAO.class);
+        System.out.println(userDAO.userList());
+
+    }
+
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        var dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://database-3.cbrtuyqxuzpi.eu-central-1.rds.amazonaws.com:3306/test");
+        dataSource.setUsername("admin");
+        dataSource.setPassword("adminadmin");
+        return new JdbcTemplate(dataSource);
+    }
+
+
+}
