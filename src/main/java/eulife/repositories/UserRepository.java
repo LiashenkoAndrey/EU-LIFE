643a268ec669df5f2add65d1f21b1994c6e996d7
Andrii Liashenko <a.liashenko@e-u.edu.ua>
2023-01-12 21:59:34 +0200

    new method findByLogin for Security

--- src/main/java/eulife/repositories/UserRepository.java
+++ src/main/java/eulife/repositories/UserRepository.java
@@ -4,6 +4,5 @@ import eulife.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
-
+    User findByLogin(String login);
 }
