d0e736535c280f68a72f00fb129675525d3c3c58
Andrii Liashenko <a.liashenko@e-u.edu.ua>
2023-01-14 10:58:34 +0200

    add new methods for creating and getting atricles

--- src/main/java/eulife/controllers/ArticleController.java
+++ src/main/java/eulife/controllers/ArticleController.java
@@ -1,17 +1,26 @@
 package eulife.controllers;
 
+import eulife.domain.Article;
+import eulife.domain.User;
 import eulife.repositories.ArticleRepository;
 import eulife.repositories.CommentRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.Date;
 
 @Controller
 @RequestMapping("/article")
 public class ArticleController {
     private final ArticleRepository articleRepository;
+
     private final CommentRepository commentRepository;
 
     public ArticleController(ArticleRepository articleRepository, CommentRepository commentRepository) {
@@ -20,10 +29,41 @@ public class ArticleController {
     }
 
 
+    @GetMapping("/new")
+    public String newArticle(Model model) {
+        model.addAttribute("article", new Article());
+        return "newArticle";
+    }
+
+
+
+    @PostMapping("/new")
+    public RedirectView postArticle(@ModelAttribute("article") Article article, Authentication auth) {
+        article.setDate_of_creation(new Date());
+        article.setAuthor((User) auth.getPrincipal());
+
+        articleRepository.save(article);
+        return new RedirectView("/");
+    }
+
+    @GetMapping("/all")
+    public String getArticles (@RequestParam(value = "page", required = false, defaultValue = "0") Integer pageIndex,
+                                      @PageableDefault(size = 2)Pageable pageable,
+                                      Model model) {
+        Page<Article> articlePages = articleRepository.findAll(pageable.withPage(pageIndex));
+        model.addAttribute("articles", articlePages.toList());
+        model.addAttribute("pagesQuantity", articlePages.getTotalPages()-1);
+        System.out.println(articlePages.toList());
+        System.out.println(articlePages.getTotalPages());
+
+        return "ArticlesList";
+    }
+
+
     @GetMapping("/{id}")
     public String showArticle(@PathVariable("id") Long id, Model model) {
-        model.addAttribute("article", articleRepository.findById(id));
-        model.addAttribute("comments", commentRepository.findCommentsByArticle_Id(id));
+        model.addAttribute("article", articleRepository.findById(id).orElseThrow(EntityNotFoundException::new));
+        model.addAttribute("comments",commentRepository.findCommentsByArticle_Id(id));
         return "article";
     }
 
