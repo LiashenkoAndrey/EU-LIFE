0325795a84c86f4064b6df67a20c8f75649dd783
Andrii Liashenko <a.liashenko@e-u.edu.ua>
2023-01-14 10:57:25 +0200

    add DATE field

--- src/main/java/eulife/domain/Article.java
+++ src/main/java/eulife/domain/Article.java
@@ -3,6 +3,8 @@ package eulife.domain;
 import jakarta.persistence.*;
 import org.springframework.stereotype.Component;
 
+import javax.xml.crypto.Data;
+import java.util.Date;
 import java.util.List;
 
 @Component
@@ -22,6 +24,45 @@ public class Article {
     @ManyToOne
     private User author;
 
-    @OneToMany(mappedBy = "article")
-    private List<Comment> comments;
+
+    public Long getId() {
+        return id;
+    }
+
+
+    private Date date_of_creation;
+
+
+
+    public Date getDate_of_creation() {
+        return date_of_creation;
+    }
+
+    public void setDate_of_creation(Date date_of_creation) {
+        this.date_of_creation = date_of_creation;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
 }
