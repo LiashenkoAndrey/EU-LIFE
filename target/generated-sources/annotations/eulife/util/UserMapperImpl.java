0eff122d2c38720220ddca383f6d18ebcff88be3
Andrii Liashenko <a.liashenko@e-u.edu.ua>
2023-01-21 16:18:39 +0200

    init

--- target/generated-sources/annotations/eulife/util/UserMapperImpl.java
+++ target/generated-sources/annotations/eulife/util/UserMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-01-20T16:48:18+0200",
+    date = "2023-01-21T16:11:12+0200",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.5 (Amazon.com Inc.)"
 )
 @Component
